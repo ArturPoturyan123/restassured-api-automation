b7d0b9255f049737a894c6ef0eea191b591af7e2
Artur Poturyan <poturyan@gmail.com>
2025-06-22 04:35:27 +0400

    added user info validator

--- src/test/java/com/api/validators/LoginValidator.java
+++ src/test/java/com/api/validators/LoginValidator.java
@@ -1,19 +1,35 @@
 package com.api.validators;
 
 import com.api.models.response.LoginResponse;
-import io.restassured.response.Response;
 
 public class LoginValidator {
 
-
-    public static void validateLoginSuccess(Response response, LoginResponse body) {
-        CommonValidator.assertStatusCode(response, 200);
-        CommonValidator.assertNotNull(body.getToken(), "Token");
-        CommonValidator.assertFieldContains(body.getMessage(), "success", "Message");
+    public static void validateSuccessfulLogin(LoginResponse response) {
+        CommonValidator.assertFieldEquals(response.getMessage(), "Login successful", "message");
+        CommonValidator.assertNotNull(response.getToken(), "token");
     }
 
-    public static void validateLoginFailure(Response response, LoginResponse body) {
-        CommonValidator.assertStatusCode(response, 400);
-        CommonValidator.assertFieldContains(body.getMessage(), "failed", "Message");
+    public static void validateUserInfo(LoginResponse response, String expectedEmail) {
+        CommonValidator.assertNotNull(response.getUser(), "user object");
+
+        CommonValidator.assertFieldEquals(response.getUser().getEmail(), expectedEmail, "email");
+        CommonValidator.assertNotNull(response.getUser().get_id(), "user._id");
+        CommonValidator.assertNotNull(response.getUser().getAge(), "user.age");
+        CommonValidator.assertNotNull(response.getUser().get__v(), "user.__v");
+        CommonValidator.assertNotNull(response.getUser().getAddress(), "user.address");
+        CommonValidator.assertNotNull(response.getUser().getName(), "user.name");
+        CommonValidator.assertNotNull(response.getUser().getCreatedAt(), "user.createdAt");
+        CommonValidator.assertNotNull(response.getUser().getUpdatedAt(), "user.updatedAt");
+    }
+
+    public static void validateInvalidLogin(LoginResponse response, String expectedMessage, String expectedDetails) {
+        CommonValidator.assertFieldEquals(response.getMessage(), expectedMessage, "message");
+        CommonValidator.assertFieldEquals(response.getDetails(), expectedDetails, "details");
+    }
+
+    public static void validateTokenFormat(String token) {
+        CommonValidator.assertNotNull(token, "token");
+        String[] parts = token.split("\\.");
+        org.testng.Assert.assertEquals(parts.length, 3, "❌ Token must follow JWT format");
     }
 }
